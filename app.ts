61ccfba11a6f78ebd8c565eafa51fa20866d1523
Filip Bech-Larsen <filipbech@filipbech.dk>
2023-11-27 22:53:12 +0100

    progress

--- app.ts
+++ app.ts
@@ -7,8 +7,17 @@ class Ahc9000 extends Homey.App {
    */
   async onInit() {
     this.log('Ahc9000 has been initialized');
+
+    const flowCard = this.homey.flow.getActionCard('greet');
+    flowCard.registerRunListener(async (response) => {
+      this.log(`hello ${response.context}`);
+    });
+
+
   }
 
-}
+
+  
+} 
 
 module.exports = Ahc9000;
